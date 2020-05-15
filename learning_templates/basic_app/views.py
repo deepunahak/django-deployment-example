37186712d600243ac840a20de8b3b0fa9222034b
deepunahak <deepunahak@gmail.com>
2020-05-15 18:50:37 +0530

    "pushing new project"

--- learning_templates/basic_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-def index(request):
-    idx_dict = {'home': 'Apllication connected!!'}
-    return(request, "basic_app/index.html", idx_dict)
-
